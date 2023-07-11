fdd468193a7878be76bc7078c89fe7c1e389ee78
B8INYAA <117551519+B8INYAA@users.noreply.github.com>
2023-07-11 20:59:57 +0200

    Delete predict.py

--- predict.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-AUTH = "INSERT_BLOXFLIP_AUTH_HERE"
-
-r = requests.post("https://api.bloxflip-predictors.xyz/v1/mines/predict", json={"auth": AUTH}).json
-
-PREDICTION = r["prediction"]
