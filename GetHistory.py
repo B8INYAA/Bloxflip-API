2dad35de94f1f19fa2f7d278019d3dc85d1b192c
B8INYAA <117551519+B8INYAA@users.noreply.github.com>
2023-07-11 20:59:32 +0200

    Delete GetHistory.py

--- GetHistory.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-AUTH = "INSERT_BLOXFLIP_AUTH_HERE"
-SIZE = 6
-
-r = requests.post("https://api.bloxflip-predictors.xyz/v1/mines/GetHistory", json={"auth": AUTH, "size": SIZE}).json
-
-DATA = r["data"]
